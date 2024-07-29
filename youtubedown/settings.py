b8bd4838642c96177270e40c4ea1c71ed12fe293
vardhan-rawat <vardhanrawat1@gmail.com>
2024-07-29 19:04:45 +0530

    update

--- youtubedown/settings.py
+++ youtubedown/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-e9y7df*ymjx_cfb2v7#s1f*)=xjh@tir*fpp-s7lke^6(&e-*b
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['https://mp4yt.azurewebsites.net']
+ALLOWED_HOSTS = ['https://mp4yt.azurewebsites.net','127.0.0.1']
 CSRF_TRUSTED_ORIGINS = [
     'https://mp4yt.azurewebsites.net',
 ]
